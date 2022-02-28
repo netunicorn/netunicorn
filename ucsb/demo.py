f25a04a5607d213bf3be70e41e91d3365195620b
Abtin <abtin@snl-server-3.cs.ucsb.edu>
2022-02-27 20:40:15 -0800

    update demo

--- ucsb/demo.py
+++ ucsb/demo.py
@@ -1,10 +1,4 @@
 import sys
-
-MINION_ID = None
-with open('/etc/salt/minion_id', 'r') as f:
-    MINION_ID = f.read()
-    print('MINION_ID is set to ', MINION_ID)
-
 import utils.minion_handler
 import utils.minion_pool
 import time
