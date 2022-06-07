35af1734b6efe9f360438b7e15438f6349bc2c93
maybe-hello-world <maybe.hello.world@gmail.com>
2022-06-07 13:49:04 -0700

    Remove unneeded files

--- pinot/library/qoe_youtube/qoe_extension/content.js
+++
@@ -1,9 +0,0 @@
-/*jshint esversion: 9 */
-
-let s = document.createElement('script');
-s.src = chrome.runtime.getURL('script.js');
-s.onload = function() {
-    "use strict";
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
