25d6eb3073e9ed241b789b0e43d0913f725f254b
maybe-hello-world <maybe.hello.world@gmail.com>
2023-02-12 01:22:56 -0800

    Rewrite compilation module to stateless version

--- netunicorn-director/netunicorn-compilation/src/netunicorn/director/compilation/api_types.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class CompilationRequest(BaseModel):
-    experiment_id: str
-    compilation_id: str
-    architecture: str
-    environment_definition: dict
-    pipeline: bytes
