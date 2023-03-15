016586635f26964cb2d9cd596c30fd33dbd230fd
maybe-hello-world <maybe.hello.world@gmail.com>
2023-03-15 00:05:49 -0700

    swap arguments of map

--- netunicorn-base/src/netunicorn/base/experiment.py
+++ netunicorn-base/src/netunicorn/base/experiment.py
@@ -43,7 +43,7 @@ class Experiment:
         self.deployment_map.append(Deployment(node, pipeline))
         return self
 
-    def map(self, nodes: Sequence[Node], pipeline: Pipeline) -> Experiment:
+    def map(self, pipeline: Pipeline, nodes: Sequence[Node]) -> Experiment:
         if isinstance(nodes, Nodes):
             raise TypeError("Expected sequence of nodes, got Nodes instead")
 
