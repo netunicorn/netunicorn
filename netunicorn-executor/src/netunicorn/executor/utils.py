daf0f559abee5658f72ce02cca48290698526a72
maybe-hello-world <maybe.hello.world@gmail.com>
2024-09-24 15:47:15 -0700

    Make executor correctly work with "spawn" instead of "fork"

--- netunicorn-executor/src/netunicorn/executor/utils.py
+++ netunicorn-executor/src/netunicorn/executor/utils.py
@@ -1,4 +1,3 @@
-import multiprocessing.pool
 import traceback
 from functools import wraps
 from typing import Any, Callable, TypeVar, Union
@@ -39,48 +38,3 @@ def safe(
             return Failure(traceback.format_exc())
 
     return decorator
-
-
-class NoDaemonProcess(multiprocessing.Process):
-    """
-    | If you try to run multiprocessing.Pool inside a multiprocessing.Pool, you'll receive this error:
-    | AssertionError: daemonic processes are not allowed to have children
-    | This class is a solution to this problem.
-    """
-
-    @property
-    def daemon(self) -> bool:
-        """
-        Returns if the process is daemon.
-        """
-        return False
-
-    @daemon.setter
-    def daemon(self, value: Any) -> None:
-        """
-        Sets the daemon value for the process.
-        """
-        pass
-
-
-class NoDaemonContext(type(multiprocessing.get_context())):  # type: ignore
-    """
-    Context for multiprocessing.Pool that uses NoDaemonProcess instead of multiprocessing.Process.
-    """
-
-    Process = NoDaemonProcess
-    """
-    Process class to use.
-    """
-
-
-class NonStablePool(multiprocessing.pool.Pool):
-    """
-    We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool because
-        the latter is only a wrapper function, not a proper class.
-    """
-
-    # noinspection PyArgumentList
-    def __init__(self, *args: Any, **kwargs: Any):
-        kwargs["context"] = NoDaemonContext()
-        super().__init__(*args, **kwargs)
